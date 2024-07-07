f42ca57a6c42f420155e477aa53d6fb403e750bc
TenzDelek <tibetdelek@gmail.com>
2024-07-07 18:24:49 +0530

    change dummy fetcher

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,22 +1,19 @@
 import Link from "next/link";
 interface Post {
   id: number;
-  title: string;
+  name: string;
   
 }
 export default async function Posts() {
-  const response = await fetch("https://dummyjson.com/posts?limit=10");
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+  const response = await fetch("https://jsonplaceholder.typicode.com/users?limit:10");
   const data = await response.json(); // converting to json
   return (
     <main className="text-center pt-16 px-5">
       <h1 className=" font-bold text-4xl md:text-5xl mb-5">ALL POSTS</h1>
       <ul>
-        {data.posts.map((post:Post) => (
+        {data.map((post:Post) => (
           <li key={post.id} className=" mb-2">
-            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            <Link href={`/posts/${post.id}`}>{post.name}</Link>
           </li>
         ))}
       </ul>
