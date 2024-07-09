3ec27f7eabc7368a29c7d9bf63b945e73662add7
TenzDelek <tibetdelek@gmail.com>
2024-07-09 16:28:08 +0530

    Formaction ui

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 export async function createpostla(formData: FormData) {
   const title = formData.get("title") as string;
-  console.log(title);
+  const body = formData.get("body") as string; // currently we are saying it is string , but we have to be carefull with this we can use zod for validation
+  console.log(title, body);
   //adding to db or updating for db
 }
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -3,20 +3,25 @@ const CreatePost = () => {
   return (
     <main className=" text-center pt-16">
       <h1 className=" text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
-      <form action={createpostla} className=" h-10 space-x-2 mt-10">
+      <form
+        action={createpostla}
+        className=" mx-auto space-y-4 max-w-[400px] flex flex-col  my-10"
+      >
         <input
           required
           placeholder=" title for new post"
           name="title"
           type="text"
-          className=" outline-none border rounded px-3 h-full"
+          className=" outline-none border rounded px-3 h-10"
         />
         <textarea
           name="body"
-          className=" border rounded px-1 h-full"
+          required
+          className=" py-2 outline-none border rounded px-3"
           placeholder="Body for new post"
+          rows={6}
         />
-        <button className=" h-full px-2  py-1 text-sm rounded bg-green-500 text-white">
+        <button className="  h-10  px-2  py-1 text-sm rounded bg-green-500 text-white">
           Submit
         </button>
       </form>
